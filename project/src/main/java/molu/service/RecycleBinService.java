2827fec086506948cf59e885c8366359d62829e7
SeiyunSky <1447909240@qq.com>
2025-07-30 17:12:43 +0800

    短链接移至回收站功能

--- project/src/main/java/molu/service/RecycleBinService.java
+++ project/src/main/java/molu/service/RecycleBinService.java
@@ -1,8 +1,13 @@
 package molu.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import molu.dao.entity.ShortLinkDO;
 import molu.dto.req.RecycleBinSaveReqDTO;
+import molu.dto.req.ShortLinkPageReqDTO;
+import molu.dto.req.ShortLinkRecycleBinPageReqDTO;
+import molu.dto.resp.ShortLinkPageRespDTO;
+
 
 /**
  * 回收短链接接口层
@@ -14,5 +19,13 @@ public interface RecycleBinService  extends IService<ShortLinkDO> {
      * @param requestParam 请求参数
      */
     void saveRecycleBin(RecycleBinSaveReqDTO requestParam);
+
+    /**
+     * 分页查询短链接
+     * @param requestParam 参数
+     * @return 参数
+     */
+    IPage<ShortLinkPageRespDTO> pageShortLink(ShortLinkRecycleBinPageReqDTO requestParam);
+
 
 }
